87f445a03d6a2e6fd1d6aab07b0840ddcedc5f8f
johnghanem961 <168030001+johnghanem961@users.noreply.github.com>
2024-06-11 12:59:31 +0300

    Delete aboutus.js

--- aboutus.js
+++
@@ -1,5 +0,0 @@
-
-document.getElementById('backBtn').addEventListener('click', function() {
-    window.location.href = 'index.html'; // Replace 'index.html' with the path to your main page
-  });
-  
