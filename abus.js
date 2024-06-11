e9a3a6f36aebb7c5e4b80c1d0ae1ce31f47a3923
johnghanem961 <168030001+johnghanem961@users.noreply.github.com>
2024-06-11 13:02:06 +0300

    Add files via upload

---
+++ abus.js
@@ -0,0 +1,5 @@
+
+document.getElementById('backBtn').addEventListener('click', function() {
+    window.location.href = 'index.html'; // Replace 'index.html' with the path to your main page
+  });
+  
